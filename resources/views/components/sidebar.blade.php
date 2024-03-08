1a5fb8dff7d45000ddc762b2e16e72cb1a616767
Isro Hidayatulloh <orsi.amax@gmail.com>
2024-03-09 00:49:21 +0700

    create user index and user create

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,15 +1,21 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="index.html">Klinik Sehat</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="active">
+            <li class="{{ Route::is('home') ? 'active' : '' }}">
                 <a class="nav-link"
-                    href="{{ url('blank-page') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+                    href="{{ route('home') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+            </li>
+        </ul>
+        <ul class="sidebar-menu">
+            <li class="{{ Route::is('users.*') ? 'active' : '' }}">
+                <a class="nav-link"
+                    href="{{ route('users.index') }}"><i class="fas fa-user"></i> <span>User</span></a>
             </li>
         </ul>
 
